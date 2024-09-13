3dfbc1930b11b7de24ac13e687d9da3d840906c3
mateoh123 <mateoche76@gmail.com>
2024-09-13 14:08:43 -0400

    .

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+ function resizeSketch(iframe, parentId, aspectRatio) {
+                    let parent = document.getElementById(parentId);
+                    let w = parent.clientWidth;
+                    iframe.width = w;
+                    iframe.height = w * aspectRatio;
+                    iframe.contentWindow.addEventListener('resize', () => {
+                        let w = parent.clientWidth;
+                        iframe.width = w;
+                        iframe.height = w * aspectRatio;
+                    });
+                }
